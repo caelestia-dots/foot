07a7b35586d790989a5ad03b161a1edc0ddb1536
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-01-26 14:52:38 +1100

    better install script
    No need to clone into ~/.config/caelestia/foot, just clone directly into foot config

--- install.fish
+++ install.fish
@@ -1,11 +1,9 @@
 #!/bin/fish
 
-set -q XDG_CONFIG_HOME && set conf $XDG_CONFIG_HOME || set conf $HOME/.config
-set config $conf/caelestia/foot
-set link $conf/foot
+set -q XDG_CONFIG_HOME && set config $XDG_CONFIG_HOME/foot || set config $HOME/.config/foot
 
 # Prompt if already exists
-if test -e $config
+if test -d $config
     read -l -p "echo -n '$config already exists. Overwrite? [y/N] '" confirm
     if test "$confirm" = 'y' -o "$confirm" = 'Y'
         echo 'Continuing.'
@@ -16,20 +14,7 @@ if test -e $config
     end
 end
 
-mkdir -p $config
-mkdir -p ~/.local/bin
 git clone 'https://github.com/caelestia-dots/foot.git' $config
+sed -i 's|$SRC|'$config'|g' $config/foot.ini
 
-if test -e $link
-    read -l -p "echo -n '$link already exists. Overwrite? [y/N] '" confirm
-    if test "$confirm" = 'y' -o "$confirm" = 'Y'
-        echo 'Continuing.'
-        rm -rf $link
-    else
-        echo 'Exiting.'
-        exit
-    end
-end
-
-cp -r $config $link
-sed -i 's|$SRC|'$link'|g' $link/foot.ini
+echo 'Done.'
