87985644bc0224704a64fa713ff62af0825be0f6
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-02-19 15:50:46 +1100

    use new scheme backend

--- colour-switcher.sh
+++ colour-switcher.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
 scheme_path="${XDG_CACHE_HOME:-$HOME/.cache}/caelestia/scheme/current.txt"
+schemes="$(dirname "$0")/schemes"
+config_path="$schemes/current.ini"
 
 update() {
-    config_path="$(dirname "$0")/schemes/$(cat "$scheme_path").ini"
-    if test -f "$config_path"; then
-        # ex: 'regular0=0b0b0b'
-        sed -n -r 's/^\w*(regular|bright)([0-9])=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}).*/\1 \2 \3 \4 \5/p' "$config_path" |
-            while read color_type idx r g b; do
-                if [ "$color_type" = 'bright' ]; then
-                    idx="$((idx + 8))"
-                fi
-                echo -ne "\e]4;$idx;rgb:$r/$g/$b\e\\"
-            done
-        # ex: 'foreground=ffffff'
-        sed -n -r 's/^\w*foreground=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}).*/\1 \2 \3/p' "$config_path" |
-            while read r g b; do
-                echo -ne "\e]10;rgb:$r/$g/$b\e\\"
-            done
-        # ex: 'background=0b0b0b'
-        sed -n -r 's/^\w*background=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}).*/\1 \2 \3/p' "$config_path" |
-            while read r g b; do
-                echo -ne "\e]11;rgb:$r/$g/$b\e\\"
-            done
-    fi
+    cp "$schemes/template.ini" "$config_path"
+
+    while read -r line || [ -n "$line" ]; do
+        local colour=( $line )
+        sed -i "s/\$${colour[0]}/${colour[1]}/g" "$config_path"
+    done < "$scheme_path"
+
+    # ex: 'regular0=0b0b0b'
+    sed -n -r 's/^\w*(regular|bright)([0-9])=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}).*/\1 \2 \3 \4 \5/p' "$config_path" |
+        while read color_type idx r g b; do
+            if [ "$color_type" = 'bright' ]; then
+                idx="$((idx + 8))"
+            fi
+            echo -ne "\e]4;$idx;rgb:$r/$g/$b\e\\"
+        done
+    # ex: 'foreground=ffffff'
+    sed -n -r 's/^\w*foreground=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}).*/\1 \2 \3/p' "$config_path" |
+        while read r g b; do
+            echo -ne "\e]10;rgb:$r/$g/$b\e\\"
+        done
+    # ex: 'background=0b0b0b'
+    sed -n -r 's/^\w*background=([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2}).*/\1 \2 \3/p' "$config_path" |
+        while read r g b; do
+            echo -ne "\e]11;rgb:$r/$g/$b\e\\"
+        done
 }
 
 update
